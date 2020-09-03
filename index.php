161795d0842a0bb0022beb0eebbea9617cd02dc2
erisilva <erivelton.contagem@gmail.com>
2020-09-03 09:30:10 -0300

    atualização das libs

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<meta charset="utf-8">
+		<meta name="robots" content="noindex, nofollow">
+	
+        <title>Secretaria Municipal de Saúde - Contagem/MG</title>
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                color: #B0BEC5;
+                display: table;
+                font-weight: 100;
+                font-family: 'Arial';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 72px;
+                margin-bottom: 40px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Secretaria Municipal de Saúde - Contagem/MG</div>
+				<div class="title">
+					
+				</div>
+            </div>
+        </div>
+    </body>
+</html>
